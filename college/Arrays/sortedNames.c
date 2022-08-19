4dd5f8b8aa09da2fd7fbf3366af345c37cbec818
jxy <90693790+jxytherealone@users.noreply.github.com>
2022-08-19 22:57:16 +0530

    Update sortedNames.c

--- college/Arrays/sortedNames.c
+++ college/Arrays/sortedNames.c
@@ -1,18 +1,23 @@
 #include<stdio.h>
 #include<string.h>
  
-int main() {
+int main() 
+{
    int i,j,n;
    char name[100][100],s[100];
    printf("Enter the number of names :\n");
    scanf("%d",&n);
    printf("Enter random names in any order:\n");
-   for(i=0;i<n;i++){
+   for(i=0;i<n;i++)
+   {
       scanf("%s",name[i]);
    }
-   for(i=0;i<n;i++){
-      for(j=i+1;j<n;j++){
-         if(strcmp(name[i],name[j])>0){
+   for(i=0;i<n;i++)
+   {
+      for(j=i+1;j<n;j++)
+      {
+         if(strcmp(name[i],name[j])>0)
+         {
             strcpy(s,name[i]);
             strcpy(name[i],name[j]);
             strcpy(name[j],s);
